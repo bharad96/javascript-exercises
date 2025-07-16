90908e52c3240d983362f99f711fa7624e9a3dcd
bharad <bharadwaj6991@gmail.com>
2025-07-16 19:44:26 +0700

    finished exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, n) {
+    if (n < 0) {
+        return "ERROR";
+    }
+    let output = '';
+    for (i = 0; i < n; i++) {
+        output += string;
+    }
+    return output;
 };
 
 // Do not edit below this line
