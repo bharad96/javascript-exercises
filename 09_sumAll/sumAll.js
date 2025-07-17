a18c4d71449796e85fd96ee5c55c240de9da03b2
bharad <bharadwaj6991@gmail.com>
2025-07-17 22:16:00 +0700

    finished exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (!Number.isInteger(start) || !Number.isInteger(end) || 
+        start < 0 || end < 0 ){
+        return 'ERROR';
+    }
+    function* generateNumbers(start, end) {
+        for (var i = start; i <= end; i++) {
+            console.log(i);
+            yield i;
+        }
+    }
+    [start, end] = [Math.min(start, end), Math.max(start, end)];
+    return generateNumbers(start, end).reduce((prev, curr) => prev + curr);
 };
 
 // Do not edit below this line
